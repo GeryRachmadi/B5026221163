a4fac7a1e7f22a4f24a9108cf8509b9b275f5060
GeryRachmadi <gerymcpe@gmail.com>
2023-12-04 01:54:13 +0700

    TESTING

--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -12,18 +12,33 @@
 	<br/>
 	<br/>
 
-	<form action="/pegawai/store" method="post" class="form-horizontal">
-		{{ csrf_field() }}
+<form action="/pegawai/store" method="post" class="form-horizontal">
+        {{ csrf_field() }}
         <div class="form-group row">
             <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
             <div class="col-xs-9">
             <input type="text" class="form-control" id="nama" name="nama">
             </div>
+        </div>
+        <div class="form-group row">
+            <label for="jabatan" class="col-xs-3 col-form-label mr-2">Jabatan</label>
+            <div class="col-xs-9">
+            <input type="text" class="form-control" id="jabatan" name="jabatan">
             </div>
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
+        </div>
+        <div class="form-group row">
+            <label for="umur" class="col-xs-3 col-form-label mr-2">Umur</label>
+            <div class="col-xs-9">
+            <input type="text" class="form-control" id="umur" name="umur">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="alamat" class="col-xs-3 col-form-label mr-2">Alamat</label>
+            <div class="col-xs-9">
+            <textarea type="text" class="form-control" id="alamat" name="alamat"></textarea>
+            </div>
+        </div>
+        <br/>
+        <input type="submit" value="Simpan Data">
+    </form>
 @endsection
